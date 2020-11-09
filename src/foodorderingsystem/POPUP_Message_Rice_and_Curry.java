f5114dfd46275276cb0633b6d1d37c5e59ab5b70
RyanAnjelo <64610012+RyanAnjelo@users.noreply.github.com>
2020-11-09 14:53:31 +0100

    up

--- src/foodorderingsystem/POPUP_Message_Rice_and_Curry.java
+++ src/foodorderingsystem/POPUP_Message_Rice_and_Curry.java
@@ -28,20 +28,19 @@ public class POPUP_Message_Rice_and_Curry extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        lblBurger = new javax.swing.JLabel();
-        lblBurgerName = new javax.swing.JLabel();
+        lblrice = new javax.swing.JLabel();
+        lblricen = new javax.swing.JLabel();
         lblLKR = new javax.swing.JLabel();
-        lblBurgerPrice = new javax.swing.JLabel();
+        lblriceprice = new javax.swing.JLabel();
         lblQTY = new javax.swing.JLabel();
-        spBurgerQty = new javax.swing.JSpinner();
+        dpriceqty = new javax.swing.JSpinner();
         lblTotalLKR = new javax.swing.JLabel();
         lblTotalPrice = new javax.swing.JLabel();
         lblTOTAL = new javax.swing.JLabel();
-        btnAddToPlateBurger = new javax.swing.JButton();
         btnCANCEL = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         btnAddToPlateBurger1 = new javax.swing.JButton();
-        btnAddToPlateBurger2 = new javax.swing.JButton();
+        Sltdropdown = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -51,65 +50,50 @@ public class POPUP_Message_Rice_and_Curry extends javax.swing.JFrame {
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Dialog", 0, 11), new java.awt.Color(0, 0, 0))); // NOI18N
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        lblBurger.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblBurger.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/rice and curry.jpg"))); // NOI18N
-        jPanel1.add(lblBurger, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 10, 250, 190));
+        lblrice.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblrice.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/rice and curry.jpg"))); // NOI18N
+        jPanel1.add(lblrice, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 10, 250, 190));
 
-        lblBurgerName.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
-        lblBurgerName.setForeground(new java.awt.Color(0, 0, 0));
-        lblBurgerName.setText("Rice and Curry");
-        jPanel1.add(lblBurgerName, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 210, 180, 30));
+        lblricen.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        lblricen.setForeground(new java.awt.Color(0, 0, 0));
+        lblricen.setText("Rice and Curry");
+        jPanel1.add(lblricen, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 210, 180, 30));
 
         lblLKR.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
         lblLKR.setForeground(new java.awt.Color(51, 51, 51));
         lblLKR.setText("LKR");
         jPanel1.add(lblLKR, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 250, 40, 20));
 
-        lblBurgerPrice.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
-        lblBurgerPrice.setForeground(new java.awt.Color(51, 51, 51));
-        lblBurgerPrice.setText("200.00");
-        jPanel1.add(lblBurgerPrice, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 250, 50, 20));
+        lblriceprice.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
+        lblriceprice.setForeground(new java.awt.Color(51, 51, 51));
+        lblriceprice.setText("200.00");
+        jPanel1.add(lblriceprice, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 250, 50, 20));
 
         lblQTY.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
         lblQTY.setForeground(new java.awt.Color(0, 0, 0));
-        lblQTY.setText("QTY");
-        jPanel1.add(lblQTY, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 350, 50, 30));
+        lblQTY.setText("QTY :");
+        jPanel1.add(lblQTY, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 330, 80, 30));
 
-        spBurgerQty.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
-        spBurgerQty.setBorder(null);
-        spBurgerQty.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jPanel1.add(spBurgerQty, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 340, 50, 50));
+        dpriceqty.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        dpriceqty.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
+        dpriceqty.setBorder(null);
+        dpriceqty.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jPanel1.add(dpriceqty, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 330, 90, 30));
 
         lblTotalLKR.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
         lblTotalLKR.setForeground(new java.awt.Color(51, 51, 51));
         lblTotalLKR.setText("LKR");
-        jPanel1.add(lblTotalLKR, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 350, 40, 30));
+        jPanel1.add(lblTotalLKR, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 390, 40, 30));
 
         lblTotalPrice.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
         lblTotalPrice.setForeground(new java.awt.Color(51, 51, 51));
         lblTotalPrice.setText("200.00");
-        jPanel1.add(lblTotalPrice, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 350, 50, 30));
+        jPanel1.add(lblTotalPrice, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 390, 50, 30));
 
         lblTOTAL.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
         lblTOTAL.setForeground(new java.awt.Color(102, 102, 102));
         lblTOTAL.setText("TOTAL :");
-        jPanel1.add(lblTOTAL, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 350, 60, 30));
-
-        btnAddToPlateBurger.setBackground(new java.awt.Color(0, 153, 0));
-        btnAddToPlateBurger.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
-        btnAddToPlateBurger.setForeground(new java.awt.Color(255, 255, 255));
-        btnAddToPlateBurger.setText("Egg");
-        btnAddToPlateBurger.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        btnAddToPlateBurger.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnAddToPlateBurger.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                btnAddToPlateBurgerMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                btnAddToPlateBurgerMouseExited(evt);
-            }
-        });
-        jPanel1.add(btnAddToPlateBurger, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 280, 110, 40));
+        jPanel1.add(lblTOTAL, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 390, 60, 30));
 
         btnCANCEL.setBackground(new java.awt.Color(255, 0, 0));
         btnCANCEL.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
@@ -128,10 +112,10 @@ public class POPUP_Message_Rice_and_Curry extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
         jLabel1.setText("Select:");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 290, 70, 30));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 280, 70, 40));
 
         btnAddToPlateBurger1.setBackground(new java.awt.Color(0, 153, 0));
-        btnAddToPlateBurger1.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        btnAddToPlateBurger1.setFont(new java.awt.Font("Algerian", 1, 24));
         btnAddToPlateBurger1.setForeground(new java.awt.Color(255, 255, 255));
         btnAddToPlateBurger1.setText("ADD TO PLATE");
         btnAddToPlateBurger1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
@@ -144,23 +128,10 @@ public class POPUP_Message_Rice_and_Curry extends javax.swing.JFrame {
                 btnAddToPlateBurger1MouseExited(evt);
             }
         });
-        jPanel1.add(btnAddToPlateBurger1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 410, 210, 60));
+        jPanel1.add(btnAddToPlateBurger1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 430, 210, 50));
 
-        btnAddToPlateBurger2.setBackground(new java.awt.Color(0, 153, 0));
-        btnAddToPlateBurger2.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
-        btnAddToPlateBurger2.setForeground(new java.awt.Color(255, 255, 255));
-        btnAddToPlateBurger2.setText("Chicken");
-        btnAddToPlateBurger2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        btnAddToPlateBurger2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnAddToPlateBurger2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                btnAddToPlateBurger2MouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                btnAddToPlateBurger2MouseExited(evt);
-            }
-        });
-        jPanel1.add(btnAddToPlateBurger2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 280, 110, 40));
+        Sltdropdown.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Chicken","Egg" }));
+        jPanel1.add(Sltdropdown, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 290, 90, -1));
 
         getContentPane().add(jPanel1);
         jPanel1.setBounds(0, 0, 400, 490);
@@ -168,14 +139,6 @@ public class POPUP_Message_Rice_and_Curry extends javax.swing.JFrame {
         setSize(new java.awt.Dimension(400, 486));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnAddToPlateBurgerMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddToPlateBurgerMouseEntered
-    
-    }//GEN-LAST:event_btnAddToPlateBurgerMouseEntered
-
-    private void btnAddToPlateBurgerMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddToPlateBurgerMouseExited
-       
-    }//GEN-LAST:event_btnAddToPlateBurgerMouseExited
 
     private void btnCANCELMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnCANCELMouseClicked
         this.setVisible(false);
@@ -188,14 +151,6 @@ public class POPUP_Message_Rice_and_Curry extends javax.swing.JFrame {
     private void btnAddToPlateBurger1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddToPlateBurger1MouseExited
         // TODO add your handling code here:
     }//GEN-LAST:event_btnAddToPlateBurger1MouseExited
-
-    private void btnAddToPlateBurger2MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddToPlateBurger2MouseEntered
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnAddToPlateBurger2MouseEntered
-
-    private void btnAddToPlateBurger2MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAddToPlateBurger2MouseExited
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnAddToPlateBurger2MouseExited
 
     /**
      * @param args the command line arguments
@@ -236,20 +191,19 @@ public class POPUP_Message_Rice_and_Curry extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAddToPlateBurger;
+    private javax.swing.JComboBox<String> Sltdropdown;
     private javax.swing.JButton btnAddToPlateBurger1;
-    private javax.swing.JButton btnAddToPlateBurger2;
     private javax.swing.JButton btnCANCEL;
+    private javax.swing.JSpinner dpriceqty;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel lblBurger;
-    private javax.swing.JLabel lblBurgerName;
-    private javax.swing.JLabel lblBurgerPrice;
     private javax.swing.JLabel lblLKR;
     private javax.swing.JLabel lblQTY;
     private javax.swing.JLabel lblTOTAL;
     private javax.swing.JLabel lblTotalLKR;
     private javax.swing.JLabel lblTotalPrice;
-    private javax.swing.JSpinner spBurgerQty;
+    private javax.swing.JLabel lblrice;
+    private javax.swing.JLabel lblricen;
+    private javax.swing.JLabel lblriceprice;
     // End of variables declaration//GEN-END:variables
 }
