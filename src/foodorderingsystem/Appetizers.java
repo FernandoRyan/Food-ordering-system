18cf24910a838e75d9e20b20b7e3ded429e9f3ea
pdssigera <67218599+pdssigera@users.noreply.github.com>
2020-11-26 19:09:22 +0530

    update

--- src/foodorderingsystem/Appetizers.java
+++ src/foodorderingsystem/Appetizers.java
@@ -16,6 +16,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 
 /**
@@ -23,12 +25,27 @@ import java.sql.Statement;
  * @author Dhanuja Supun
  */
 public class Appetizers extends javax.swing.JFrame {
+    
+    //Creating fields
+    public double Total=0.0;
+    int count;
+    
+    //Connection settings to database
+    private Connection conn;
+    private String connectionUrl = "jdbc:mysql://localhost:3306/foodorderingsystem";
+    private String username= "sa";
+    private String Pass="anjalo9990";
+    
 
     /**
      * Creates new form Appetizers
      */
     public Appetizers() {
         initComponents();
+        Displayorder() ;
+        FormatTable();
+        CheckTable();
+        GetTotal();
     }
 
     /**
@@ -515,6 +532,7 @@ public class Appetizers extends javax.swing.JFrame {
         Meal ml = new Meal();
         ml.setVisible(true);
         this.hide();
+        
     }//GEN-LAST:event_btnMealsMousePressed
 
     private void btnFastfoodsMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnFastfoodsMouseEntered
@@ -538,6 +556,7 @@ public class Appetizers extends javax.swing.JFrame {
         Fastfoods fd = new Fastfoods();
         fd.setVisible(true);
         this.hide();
+        
     }//GEN-LAST:event_btnFastfoodsMousePressed
 
     private void btnAppetizersMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAppetizersMouseEntered
@@ -559,6 +578,7 @@ public class Appetizers extends javax.swing.JFrame {
 
     private void btnAppetizersMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnAppetizersMousePressed
        this.setVisible(true);
+       
     }//GEN-LAST:event_btnAppetizersMousePressed
 
     private void btnBeveragesMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnBeveragesMouseEntered
@@ -578,6 +598,12 @@ public class Appetizers extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_btnBeveragesMouseExited
 
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {                                  
+        // This display the Frame into middle of the screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize(screenSize.width, screenSize.height);
+    }
+    
     private void btnBeveragesMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnBeveragesMousePressed
         Beverages bv = new Beverages();
         bv.setVisible(true);
@@ -588,6 +614,11 @@ public class Appetizers extends javax.swing.JFrame {
 
         POPUP_Message_Chicken_CheeseBalls cc = new POPUP_Message_Chicken_CheeseBalls();
         cc.setVisible(true);
+        
+        DefaultTableModel model = (DefaultTableModel) Ordertable.getModel();
+        model.setRowCount(0);
+        Displayorder();
+        GetTotal();
 
     }//GEN-LAST:event_lblChickenCheeseBallsMousePressed
 
@@ -595,6 +626,11 @@ public class Appetizers extends javax.swing.JFrame {
 
         POPUP_Message_Wade w = new POPUP_Message_Wade();
         w.setVisible(true);
+        
+        DefaultTableModel model = (DefaultTableModel) Ordertable.getModel();
+        model.setRowCount(0);
+        Displayorder();
+        GetTotal();
 
     }//GEN-LAST:event_lblWadeMousePressed
 
@@ -602,6 +638,11 @@ public class Appetizers extends javax.swing.JFrame {
 
         POPUP_Message_PotatoChips pc = new POPUP_Message_PotatoChips();
         pc.setVisible(true);
+        
+        DefaultTableModel model = (DefaultTableModel) Ordertable.getModel();
+        model.setRowCount(0);
+        Displayorder();
+        GetTotal();
        
     }//GEN-LAST:event_lblPotatoChipsMousePressed
 
@@ -609,6 +650,11 @@ public class Appetizers extends javax.swing.JFrame {
 
         POPUP_Message_Samosa s = new POPUP_Message_Samosa();
         s.setVisible(true);
+        
+        DefaultTableModel model = (DefaultTableModel) Ordertable.getModel();
+        model.setRowCount(0);
+        Displayorder();
+        GetTotal();
        
     }//GEN-LAST:event_lblSamosaMousePressed
 
@@ -616,6 +662,11 @@ public class Appetizers extends javax.swing.JFrame {
 
         POPUP_Message_CheeseBalls cb = new POPUP_Message_CheeseBalls();
         cb.setVisible(true);
+        
+        DefaultTableModel model = (DefaultTableModel) Ordertable.getModel();
+        model.setRowCount(0);
+        Displayorder();
+        GetTotal();
 
         // TODO add your handling code here:
     }//GEN-LAST:event_lblCheeseBallsMousePressed
