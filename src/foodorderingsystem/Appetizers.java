ebbf68277e57bf142ad412f4a04dce3b065f4c8e
pdssigera <67218599+pdssigera@users.noreply.github.com>
2020-11-28 19:04:32 +0530

    update

--- src/foodorderingsystem/Appetizers.java
+++ src/foodorderingsystem/Appetizers.java
@@ -411,17 +411,9 @@ public class Appetizers extends javax.swing.JFrame implements MainInterface {
 
             },
             new String [] {
-                "Product Description", "QTY", "Total"
+                "ItemNo", "Product Description", "QTY", "Total"
             }
-        ) {
-            Class[] types = new Class [] {
-                java.lang.String.class, java.lang.Integer.class, java.lang.Double.class
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-        });
+        ));
         jScrollPane1.setViewportView(Ordertable);
 
         btnTrash.setBackground(new java.awt.Color(255, 255, 255));
